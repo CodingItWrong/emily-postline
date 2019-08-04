66616bc3abb47101089b37027f00e48495c1c6fa
Josh Justice <me@codingitwrong.com>
2019-08-04 13:40:45 -0400

    Add sender field

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,6 +53,6 @@ class PostsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def post_params
-      params.require(:post).permit(:message)
+      params.require(:post).permit(:message, :sender)
     end
 end
